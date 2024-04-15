8e83b86e3fe9d6950a11d0ebffa3a1f1f7a5c700
Nawel <nawel.dayrek@etu.univ-amu.fr>
2024-04-15 09:34:50 +0200

    TP2 Exercice 3

--- src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -1,17 +1,77 @@
 package fr.amu.iut.exercice3;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class MaPremiereFenetreJavaFX extends Application {
+    // Label affichant le message de bienvenue
+    private Label helloLabel;
+
+    // Champ de saisi du nom de l'utilisateur
+    private TextField nameField;
+
+    // Bouton déclenchant la mise à jour du texte
+    private Button button;
+
+    // Gestionnaire d'évènements appelé lors du clic sur le bouton
+    EventHandler<MouseEvent> buttonClickHandler = actionEvent -> {
+        helloLabel.setText( "Bonjour à toi, "+nameField.getText() );
+    };
 
     public static void main(String[] args) {
+
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("La page d'un Pro de JavaFX");
+        VBox vbox = new VBox();
+        vbox.setAlignment( Pos.CENTER );
+
+        // Création et ajout du label au conteneur
+        Label helloLabel = new Label("Bonjour à tous !");
+        vbox.getChildren().add( helloLabel );
+
+        // Création de la scene
+        Scene scene = new Scene( vbox );
+
+        // Ajout de la scene à la fenêtre
+        primaryStage.setScene( scene );
+
+        // Ajout d'un champ de saisi de texte de taille 180 pixels
+        TextField nameField = new TextField("Veuillez saisir un nom");
+        nameField.setMaxWidth(180.0d);
+        nameField.setFont( Font.font("Courier", FontWeight.NORMAL, 12) );
+        vbox.getChildren().add( nameField );
+
+        // Ajout d'un bouton avec du texte
+        Button button = new Button();
+        vbox.getChildren().add( button );
+        Image image = new Image( "exercice3/Bonjour.jpg");
+        ImageView iv = new ImageView();
+        iv.setImage(image);
+        button.setGraphic( iv );
+
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            helloLabel.setText( "Bonjour à toi, "+nameField.getText() );
+        });
+
+        primaryStage.setTitle("Hello application");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
         primaryStage.show();
     }
 }
